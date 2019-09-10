bc2d66f2cb3f2f1a4946dce1e2d0d6442e8f14bd
Michael Gooch <michael_gooch@harvardpilgrim.org>
2019-09-10 18:19:41 -0400

    partial implementation being stored for further work to be done at home

---
+++ R/multi_process_mediate.R
@@ -0,0 +1,16 @@
+
+simulate_and_mediate <- function(med_model_vars){
+  
+}
+
+mediate_parallel.unix <- function(){
+  
+}
+
+mediate_parallel.non_unix <- function(){
+  
+}
+
+mediate_parallel <- function(){
+  
+}
